8a72c578d5ce1ce2b7dd453bf3af2120c19b9bdd
kappa15 <rodrigotomasio3@gmail.com>
2024-10-31 17:28:46 +0000

    .

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("click",(event)=>{
-    console.log(event.target);
     if(event.target.className == "analises"){
         document.querySelector("#popup").innerHTML = event.target.querySelector(".desc").innerHTML;
         document.querySelector("#popup").classList.add("pop_ativo");
@@ -11,5 +10,12 @@ document.addEventListener("keydown",(event) =>{
         if(document.querySelector("#popup").className == "pop_ativo"){
             document.querySelector("#popup").classList.remove("pop_ativo");
         }
+    }
+})
+
+document.addEventListener("click",(event)=>{
+    console.log(event.target);
+    if(event.target.className == "sair"){
+        document.querySelector("#popup").classList.remove("pop_ativo");
     }
 })
